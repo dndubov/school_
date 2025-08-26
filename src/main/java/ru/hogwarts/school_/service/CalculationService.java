9ec55dd5159e235bcda45b36e779941543860480
Dmitriy <dmitriyndubov@gmail.com>
2025-08-26 16:19:31 +0500

    Adjustments have been made according to comments.

--- src/main/java/ru/hogwarts/school_/service/CalculationService.java
+++ src/main/java/ru/hogwarts/school_/service/CalculationService.java
@@ -3,16 +3,18 @@ package ru.hogwarts.school_.service;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.stream.IntStream;
+
 @Service
 @Slf4j
 public class CalculationService {
 
     public int sumMillionNumbers() {
-        log.info("Вычисление суммы чисел от 1 до 1_000_000 начато");
-
-        int n = 1_000_000;
-        int sum = n * (n + 1) / 2;
-
+        log.info("Вычисление суммы чисел от 1 до 1_000_000 начато (параллельно)");
+        int sum = IntStream.iterate(1, a -> a + 1)
+                .limit(1_000_000)
+                .parallel()          // делаем стрим параллельным
+                .reduce(0, Integer::sum);
         log.info("Вычисление завершено. Сумма = {}", sum);
         return sum;
     }
